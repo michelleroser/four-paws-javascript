db0a4881be6a81354e2d12bf2faec19d900c2e49
michelleroser <82451439+michelleroser@users.noreply.github.com>
2021-11-16 21:21:51 +0000

    Added Email JS functionality to contact form

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+    emailjs.send("service_ryfuspq", "enquiry_form", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+
+
+}
