c6768fc8d1e8ee2c073304d98961eaa0be8bce17
michelleroser <82451439+michelleroser@users.noreply.github.com>
2021-11-18 20:32:20 +0000

    Added weather widget to contact page to show weather forecast over 7 days, styled the map labels and icon for marker

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -20,6 +20,7 @@ function initMap() {
         { lat: 51.251751, lng: -0.199581 }
     ];
 
+
     const svgMarker = {
         path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
         fillColor: "pink",
@@ -31,13 +32,31 @@ function initMap() {
       };
 
 
-    var markers = locations.map(function(location, i) {
+    const markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
-            label: labels[i % labels.length],
+            label: {
+              text: labels[i % labels.length],
+              color: '#fff',
+              fontSize: '15px',
+              fontWeight: 'bold',
+                  },
             icon: svgMarker,
+            title: "Click to zoom",
         });
-    });
+
+
+   //  map.addListener("center_changed", () => {
+   //   window.setTimeout(() => {
+   //     map.panTo(marker.getPosition());
+   //   }, 3000);
+   // });
+   // marker.addListener("click", () => {
+   //   map.setZoom(20);
+   //   map.setCenter(marker.getPosition());
+   // });
+
+  });
 
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
